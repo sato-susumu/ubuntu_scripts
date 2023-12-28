f4d109b7aab94151592b1d61e6722a46feb0bc2b
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2023-12-28 11:11:33 +0900

    update

--- install_ros_humble_packages.sh
+++ install_ros_humble_packages.sh
@@ -11,37 +11,13 @@ sudo apt install -y ros-humble-navigation2
 sudo apt install -y ros-humble-nav2-bringup
 sudo apt install -y ros-humble-xacro
 
-sudo apt remove -y ros-humble-dynamixel-sdk
-sudo apt remove -y ros-humble-turtlebot3-msgs
-sudo apt remove -y ros-humble-turtlebot3*
-
+sudo apt install -y ros-humble-dynamixel-sdk
+sudo apt install -y ros-humble-turtlebot3
+sudo apt install -y ros-humble-turtlebot3-msgs
 sudo apt install -y ros-humble-turtlebot3-gazebo
-
-mkdir -p ~/turtlebot3_ws/src
-cd ~/turtlebot3_ws/src/
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone -b humble-jp-devel https://github.com/ROBOTIS-JAPAN-GIT/turtlebot3_jp_custom.git
-cd ~/turtlebot3_ws
-colcon build --symlink-install
-echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
-source ~/.bashrc
-
+sudo apt install -y ros-humble-turtlebot3-fake-node
 sudo apt install -y ros-humble-realsense2-camera
-cd ~/turtlebot3_ws/src
-git clone -b humble-jp-devel https://github.com/ROBOTIS-JAPAN-GIT/realsense-ros_jp_custom
-git clone -b foxy-devel https://github.com/pal-robotics/realsense_gazebo_plugin
-cd ~/turtlebot3_ws
-colcon build --symlink-install
 
-cd ~/turtlebot3_ws/src/
-git clone -b humble-jp-devel https://github.com/ROBOTIS-JAPAN-GIT/turtlebot3_simulations_jp_custom
-git clone https://github.com/robotics-upo/lightsfm
-cd lightsfm
-make
-sudo make install
-cd ~/turtlebot3_ws
-colcon build --symlink-install
 
 echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
 echo "export LDS_MODEL=LDS-01" >> ~/.bashrc
