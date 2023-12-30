e038a26066df40bf6e862bc8ccc33fa71c42a765
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2023-12-30 15:45:58 +0900

    update

--- just_for_me.sh
+++ just_for_me.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ./update_bashrc_with_date_delimiter.sh
-
+./update_bashrc_with_current_path.sh
+./update_bashrc_with_bell_off.sh
 ./sudo_nopasswd_add_current_user.sh
-
 ./user_dirs_normalization.sh
-
 ./gnome_screensaver_lock_off.sh
-./install_autoware.sh
+
+# ./install_autoware.sh
 ./install_docker.sh
 ./install_nvidia_container_toolkit.sh
 ./install_ssh_server.sh
@@ -15,15 +15,13 @@
 ./install_chrome.sh
 ./install_realsense_sdk.sh  
 
-./install_pycharm_professional.sh
+# ./install_pycharm_professional.sh
 
 ./install_aws_cli.sh
 ./install_gcloud_cli.sh
 ./install_github_cli.sh
 ./install_utilities.sh
 
-./update_bashrc_with_current_path.sh
-./update_bashrc_with_bell_off.sh
 
 echo -e "\e[31mIMPORTANT: Please log out and then log back in.\e[0m"
 
