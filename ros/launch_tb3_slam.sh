1fccefc821e5ad65ada9b9656b5f25c2f381f291
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2023-12-31 19:04:56 +0900

    update

--- ros/launch_tb3_slam.sh
+++ ros/launch_tb3_slam.sh
@@ -8,3 +8,7 @@ launch_command 'ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py'
 
 # launch_command 'ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/map.yaml'
 launch_command 'ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True slam:=True'
+launch_command 'ros2 run turtlebot3_teleop teleop_keyboard'
+
+echo "save map"
+echo "   ros2 run nav2_map_server map_saver_cli -f ~/map"
