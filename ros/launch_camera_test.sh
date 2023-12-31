0378686461c617418bc3c8bf7be8aeac130a0629
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2023-12-31 21:17:33 +0900

    update

--- ros/launch_camera_test.sh
+++ ros/launch_camera_test.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# Function to start ROS node if the device exists
-start_ros_node_if_device_exists() {
-    if [ -e "/dev/$1" ]; then
-        gnome-terminal --tab -- bash -c "ros2 run v4l2_camera v4l2_camera_node --ros-args -p video_device:=/dev/$1; exec bash"
-    else
-        echo "/dev/$1 does not exist"
-    fi
-}
+./launch_camera.sh
 
 launch_command() {
     gnome-terminal --tab -- bash -c "$1; exec bash"
 }
 
-# Check for the existence of devices and start ROS nodes
-start_ros_node_if_device_exists "video0"
-# start_ros_node_if_device_exists "video2"
-# start_ros_node_if_device_exists "video4"
-# start_ros_node_if_device_exists "video6"
-# start_ros_node_if_device_exists "video8"
-# start_ros_node_if_device_exists "device10"
-
 launch_command 'ros2 run rqt_image_view rqt_image_view'
+launch_command 'ros2 topic hz /image_raw'
+launch_command 'ros2 topic bw /image_raw'
