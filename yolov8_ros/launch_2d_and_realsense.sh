4936a638f2455eaf35fa8127284fb2b216f12f36
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2024-01-01 12:39:08 +0900

    update

--- yolov8_ros/launch_2d_and_realsense.sh
+++ yolov8_ros/launch_2d_and_realsense.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# Check if xterm is installed and install it if not
+if ! command -v xterm &> /dev/null; then
+    echo "xterm is not installed. Installing xterm..."
+    sudo apt-get update && sudo apt-get install -y xterm
+fi
+
+# Function to execute a command in a new xterm window
 launch_command() {
-    gnome-terminal --tab -- bash -c "$1; exec bash"
+    xterm -e bash -c "$1; exec bash" &
 }
+
+# Execute the commands in new xterm windows
 
 # For details, please refer to the URL below.
 # https://github.com/IntelRealSense/realsense-ros
+# https://github.com/mgonzs13/yolov8_ros
 
 source ~/yolov8_ros/install/setup.bash
 
-launch_command 'ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=false enable_color:=true enable_depth:=true'
+launch_command "source ~/yolov8_ros/install/setup.bash"
+launch_command "ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=false enable_color:=true enable_depth:=true"
+launch_command "ros2 launch yolov8_bringup yolov8.launch.py input_image_topic:=/camera/color/image_raw"
+# launch_command "ros2 run rqt_image_view rqt_image_view"
+# launch_command "rqt_graph"
+launch_command "bash -c 'ros2 run rqt_image_view rqt_image_view & rqt_graph'"
+launch_command "echo 'nodes:'; ros2 node list; echo ''; echo 'topics:'; ros2 topic list -t; echo ''"
 
-launch_command 'ros2 launch yolov8_bringup yolov8.launch.py input_image_topic:=/camera/color/image_raw'
-launch_command 'ros2 run rqt_image_view rqt_image_view'
-launch_command 'rqt_graph'
-launch_command '(
-echo "nodes:";
-ros2 node list;
-echo "";
-echo "topics:";
-ros2 topic list -t;
-echo "";
-)'
+# Wait for key input
+read -p "Press any key to exit all xterm windows..."
 
-
+# Kill all xterm processes
+killall xterm
---
+++ yolov8_ros/launch_3d_and_realsense.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+launch_command() {
+    gnome-terminal --tab -- bash -c "$1; exec bash"
+}
+
+# For details, please refer to the URL below.
+# https://github.com/IntelRealSense/realsense-ros
+
+source ~/yolov8_ros/install/setup.bash
+
+launch_command 'ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=false enable_color:=true enable_depth:=true'
+
+launch_command 'ros2 launch yolov8_bringup yolov8_3d.launch.py input_image_topic:=/camera/color/image_raw input_depth_topic:=/camera/depth/image_rect_raw input_depth_info_topic:=/camera/depth/camera_info'
+launch_command 'ros2 run rqt_image_view rqt_image_view'
+launch_command 'rqt_graph'
+launch_command '(
+echo "nodes:";
+ros2 node list;
+echo "";
+echo "topics:";
+ros2 topic list -t;
+echo "";
+)'
+
+
