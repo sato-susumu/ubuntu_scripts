278505cdbee9932fe13efefbe859233aeaa2acec
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2024-01-03 15:54:38 +0900

    update

--- realsense_ros/launch_point_cloud.sh
+++ realsense_ros/launch_point_cloud.sh
@@ -7,6 +7,9 @@ launch_command() {
 # For details, please refer to the URL below.
 # https://github.com/IntelRealSense/realsense-ros
 
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map odom" "odom"
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom base_link" "base_lin"
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_link" "camera_link"
 launch_command "ros2 launch realsense2_camera rs_launch.py pointcloud.enable:=true" "realsense2_camera"
 
 
@@ -21,11 +24,12 @@ echo "";
 
 launch_command "ros2 topic hz /camera/color/image_raw" "topic hz image_raw"
 launch_command "ros2 topic hz /camera/aligned_depth_to_color/image_raw" "topic hz aligned depth image_raw"
-launch_command "ros2 topic hz /yolo/dbg_image" "topic hz dbg_image"
+launch_command "ros2 topic hz /camera/depth/color/points" "topic hz points"
 
 launch_command "rqt_graph" "rqt_graph"
 launch_command "ros2 run rqt_tf_tree rqt_tf_tree --force-discover"
 launch_command "ros2 run rqt_image_view rqt_image_view" "rqt_image_view"
+launch_command "rviz2 -d launch_point_cloud.rviz" "rviz2"
 
 # Wait for key input
 read -p "Press any key to exit all xterm windows..."
