db656cd7f823a095de7e87a2e5888015627d2dd8
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2024-01-03 17:34:45 +0900

    update

--- yolov8_ros/launch_rs_and_3d.sh
+++ yolov8_ros/launch_rs_and_3d.sh
@@ -19,17 +19,19 @@ launch_command() {
 
 source ~/yolov8_ros/install/setup.bash
 
-launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_link"
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map odom" "odom"
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom base_link" "base_lin"
+launch_command "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_link" "camera_link"
 
-launch_command "ros2 launch realsense2_camera rs_launch.py enable_sync:=true align_depth.enable:=true enable_color:=true enable_depth:=true" "RealSense Camera"
+launch_command "ros2 launch realsense2_camera rs_launch.py enable_sync:=true align_depth.enable:=true enable_color:=true enable_depth:=true pointcloud.enable:=true disparity_filter.enable:=true spatial_filter.enable:=true temporal_filter.enable:=true hole_filling_filter.enable:=truedecimation_filter.enable:=true" "realsense2_camera"
 launch_command "ros2 launch yolov8_bringup yolov8_3d.launch.py input_image_topic:=/camera/color/image_raw input_depth_topic:=/camera/aligned_depth_to_color/image_raw input_depth_info_topic:=/camera/aligned_depth_to_color/camera_info model:=$1" "YoloV8 3D"
-launch_command "ros2 run rqt_image_view rqt_image_view" "rqt_image_view"
-launch_command "rqt_graph" "rqt_graph"
 launch_command "echo 'nodes:'; ros2 node list; echo ''; echo 'topics:'; ros2 topic list -t; echo ''; sleep 3; nvidia-smi" "ros2 command"
 launch_command "ros2 topic hz /camera/color/image_raw" "topic hz image_raw"
 launch_command "ros2 topic hz /camera/aligned_depth_to_color/image_raw" "topic hz aligned depth image_raw"
 launch_command "ros2 topic hz /yolo/dbg_image" "topic hz dbg_image"
-launch_command "ros2 run rqt_tf_tree rqt_tf_tree --force-discover"
+# launch_command "ros2 run rqt_tf_tree rqt_tf_tree --force-discover"
+# launch_command "rqt_graph" "rqt_graph"
+launch_command "rviz2 -d launch_rs_and_3d.rviz" "rviz2"
 
 # Wait for key input
 read -p "Press any key to exit all xterm windows..."
